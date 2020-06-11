329d05f062051882266bf3e6ea59f022a0ed2920
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-06-11 17:45:24 -0400

    proving cdkeditor for blogs

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import injectContext from './store/appContext';
+import TextEditor from './components/texteditor';
 
 const App = props => {
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <div className="row">
+        <div className="col-md-4">
+          <h1>Texto 1</h1>
+          <TextEditor />
+        </div>
+      </div>
+      <br />
+      <h1>Texto 2</h1>
+      <TextEditor />
+      <br />
+      <h1>Texto 3</h1>
+      <TextEditor />
+      <br />
+      <h1>Texto 4</h1>
+      <TextEditor />
     </div>
   );
 }
--- front/src/index.js
+++ front/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
 
 ReactDOM.render(
   <React.StrictMode>
