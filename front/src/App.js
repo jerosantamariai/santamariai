9c6ca56d19e269ddfa23204ec3b7617d2fb0d3d7
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-06-12 22:26:00 -0400

    default navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import injectContext from './store/appContext';
+import Navbar from './components/navbar';
 import TextEditor from './components/texteditor';
 
 const App = props => {
   return (
     <div className="App">
+      <Navbar />
       <div className="row">
         <div className="col-md-4">
           <h1>Texto 1</h1>
