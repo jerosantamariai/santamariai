a97841fa3fdfc2b66feefd2587b6dd7e53eb91ae
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-06-12 22:28:26 -0400

    new table on models for users

--- back/models.py
+++ back/models.py
@@ -1,3 +1,25 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+class Users (db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+    lastname = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(100), nullable=False)
+    avatar = db.Column(db.String(100), nullable=True, default='defaultavatar.jpg')
+    phone = db.Column(db.String(12), nullable=True)
+    createdate = db.Column(db.DateTime, default=datetime.now())
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "email": self.email,
+            "avatar": self.avatar,
+            "phone": self.phone,
+            "createdate": self.createdate,
+        }
