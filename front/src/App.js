99d057f102e9ace870921f0809f0102718238da5
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-06-15 20:15:59 -0400

    setting the first css configurations and navbar and footer ready

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import injectContext from './store/appContext';
 import Navbar from './components/navbar';
-import TextEditor from './components/texteditor';
 import Home from './views/home';
 import NotFound from './views/notfound';
 import Footer from './components/footer';
--- front/src/components/footer.js
+++ front/src/components/footer.js
@@ -6,36 +6,36 @@ const Footer = props => {
         <>
             <footer className="generalbg">
                 <div className="container">
-                    <div className="row text-center d-flex justify-content-center pt-5 mb-3">
-                        <div className="col-md-2 mb-3">
-                            <h6 className="text-uppercase font-weight-bold">
+                    <div className="row text-center d-flex justify-content-center pt-4 my-2">
+                        <div className="col-md-2 my-2">
+                            <h6 className="text-uppercase font-weight-bold noDecoration">
                                 <Link to="#!">About us</Link>
                             </h6>
                         </div>
-                        <div className="col-md-2 mb-3">
-                            <h6 className="text-uppercase font-weight-bold">
+                        <div className="col-md-2 my-2">
+                            <h6 className="text-uppercase font-weight-bold noDecoration">
                                 <Link to="#!">Products</Link>
                             </h6>
                         </div>
-                        <div className="col-md-2 mb-3">
-                            <h6 className="text-uppercase font-weight-bold">
+                        <div className="col-md-2 my-2">
+                            <h6 className="text-uppercase font-weight-bold noDecoration">
                                 <Link to="#!">Awards</Link>
                             </h6>
                         </div>
-                        <div className="col-md-2 mb-3">
-                            <h6 className="text-uppercase font-weight-bold">
+                        <div className="col-md-2 my-2">
+                            <h6 className="text-uppercase font-weight-bold noDecoration">
                                 <Link to="#!">Help</Link>
                             </h6>
                         </div>
-                        <div className="col-md-2 mb-3">
-                            <h6 className="text-uppercase font-weight-bold">
+                        <div className="col-md-2 my-2">
+                            <h6 className="text-uppercase font-weight-bold noDecoration">
                                 <Link to="#!">Contact</Link>
                             </h6>
                         </div>
                     </div>
-                    <hr className="rgba-white-light" />
+                    <hr className="hrsolid" />
                     <div className="row d-flex text-center justify-content-center mb-md-0 mb-4">
-                        <div className="col-md-8 col-12 mt-5">
+                        <div className="col-md-8 col-12 my-2">
                             <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                             accusantium doloremque laudantium, totam rem
                             aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
@@ -43,34 +43,28 @@ const Footer = props => {
                             Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
                         </div>
                     </div>
-                    <hr className="clearfix d-md-none rgba-white-light" />
-                    <div className="row pb-3">
+                    <hr className="hrsolid" />
+                    <div className="row py-2">
                         <div className="col-md-12">
-                            <div className="mb-5 flex-center">
-                                <Link className="fb-ic">
-                                    <i className="fab fa-facebook-f fa-lg white-text mr-4"> </i>
-                                </Link>
-                                <Link className="tw-ic">
-                                    <i className="fab fa-twitter fa-lg white-text mr-4"> </i>
-                                </Link>
-                                <Link className="gplus-ic">
-                                    <i className="fab fa-google-plus-g fa-lg white-text mr-4"> </i>
-                                </Link>
-                                <Link className="li-ic">
-                                    <i className="fab fa-linkedin-in fa-lg white-text mr-4"> </i>
-                                </Link>
-                                <Link className="ins-ic">
-                                    <i className="fab fa-instagram fa-lg white-text mr-4"> </i>
-                                </Link>
-                                <Link className="pin-ic">
+                            <div className="mb-4 d-flex justify-content-center">
+                                <a href="http://www.facebook.com" target="_blank">
+                                    <i className="fab fa-facebook-f fa-lg white-text mr-5"> </i>
+                                </a>
+                                <a href="http://www.twitter.com" target="_blank">
+                                    <i className="fab fa-twitter fa-lg white-text mr-5"> </i>
+                                </a>
+                                <a href="http://www.linkedin.com" target="_blank">
+                                    <i className="fab fa-linkedin-in fa-lg white-text mr-5"> </i>
+                                </a>
+                                <a href="http://www.instagram.com" target="_blank">
+                                    <i className="fab fa-instagram fa-lg white-text mr-5"> </i>
+                                </a>
+                                <a href="http://www.pinterest.com" target="_blank">
                                     <i className="fab fa-pinterest fa-lg white-text"> </i>
-                                </Link>
+                                </a>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="footer-copyright text-center py-3">© 2020 Copyright:
-                    <Link to="https://mdbootstrap.com/"> MDBootstrap.com</Link>
                 </div>
             </footer>
 
