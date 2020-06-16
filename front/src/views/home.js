58b9feb7f23ae76e483e1106a2e0779c3dfe66fd
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-06-15 22:25:29 -0400

    inserting a carousel model

--- front/src/views/home.js
+++ front/src/views/home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import HomeCarousel from '../components/homecarousel';
 
 const Home = props => {
-    return(
-        <h1>HOME!!!</h1>
+    return (
+        <>
+            <h1>HOME!!!</h1>
+            <HomeCarousel />
+        </>
     );
 }
 
