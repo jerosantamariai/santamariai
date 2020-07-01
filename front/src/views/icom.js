9a3607da41ca2f81d5f4b2ea534b7fbc5eb2f85d
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-06-30 21:01:11 -0400

    prepared icomblog

--- front/src/views/icom.js
+++ front/src/views/icom.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../store/appContext';
+import { Link } from 'react-router-dom';
 
 const Icom = props => {
+    const { store, actions } = useContext(Context)
     return (
         <>
             <h1>ICOM</h1>
-            <div className="card-columns">
-                <div className="card">
-                    <img src="https://picsum.photos/300/400" className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title that wraps to a new line</h5>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div className="blog-container">
+                <div className="row">
+                    <div className="card-deck">
+                        {
+                            !!store.icomblog ?
+                                store.icomblog.map((icomblo, i) => {
+                                    return (
+                                        <div className="col-lg-3 col-md-4 col-xs-12">
+                                            <div className="card mb-2 blogcard" key={i}>
+                                                <Link type="button" className="btn btn-light" to={"/fstack/" + icomblo.fsurl}>
+                                                    <img src={icomblo.icomfoto} className="card-img-top my-2" alt={"image of " + icomblo.icomtitulo} />
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">{icomblo.icomtitulo}</h5>
+                                                        <p className="card-text text-justify">{icomblo.icomdescripcion}</p>
+                                                    </div>
+                                                    <div className="align-bottom">
+                                                        <small className="text-muted">{icomblo.icomcreatedate}</small>
+                                                    </div>
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                                ) : (
+                                    <div className="text-center" id="undefined">
+                                        <div className="spinner-grow text-light" role="status">
+                                        </div>
+                                    </div>
+                                )
+                        }
                     </div>
                 </div>
             </div>
