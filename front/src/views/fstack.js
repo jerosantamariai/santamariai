008d2d07da782cd02338620a0280fae1fc60f046
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-06-30 20:42:19 -0400

    fixed problem with the responsivity and the alignment in the blogs and now its ready to copypaste

--- front/src/views/fstack.js
+++ front/src/views/fstack.js
@@ -9,20 +9,20 @@ const Fstack = props => {
             <h1>Full Stack</h1>
             <div className="blog-container">
                 <div className="row">
-                    <div className="card-deck justify-content-center">
+                    <div className="card-deck">
                         {
                             !!store.fsblog ?
                                 store.fsblog.map((fsblo, i) => {
                                     return (
                                         <div className="col-lg-3 col-md-4 col-xs-12">
-                                            <div className="card h-100 mb-2 blogcard" key={i}>
+                                            <div className="card mb-2 blogcard" key={i}>
                                                 <Link type="button" className="btn btn-light" to={"/fstack/" + fsblo.fsurl}>
                                                     <img src={fsblo.fsfoto} className="card-img-top my-2" alt={"image of " + fsblo.fstitulo} />
                                                     <div className="card-body">
                                                         <h5 className="card-title">{fsblo.fstitulo}</h5>
                                                         <p className="card-text text-justify">{fsblo.fsdescripcion}</p>
                                                     </div>
-                                                    <div className="card-footer">
+                                                    <div className="align-bottom">
                                                         <small className="text-muted">{fsblo.fscreatedate}</small>
                                                     </div>
                                                 </Link>
